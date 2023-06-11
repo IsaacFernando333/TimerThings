bf646f108ebee6ecb7dea02dddfd3c55f211c6e0
Isaac Fernando <isaacfernandonovo@gmail.com>
2023-06-11 20:14:06 -0300

    11062023

--- js/efeitos.js
+++ js/efeitos.js
@@ -6,104 +6,68 @@ const clean = document.querySelector('.zerar');
 const principal = document.querySelector('.principal');
 const conteudo = document.querySelector('.conteudo');
 const timer = document.querySelector('.timer');
-const temasDiv = document.querySelectorAll('.temas-div');
+const temasDiv = document.querySelector('.temas-div');
 const temas = document.querySelector('.temas');
-const temas2 = document.querySelector('.temas-2');
+const iconePincel = document.querySelector('.icone-pincel');
 
 // temas
 const blue = document.querySelector('.blue');
 const pink = document.querySelector('.pink');
 const dark = document.querySelector('.dark');
 
+
 document.addEventListener('click', function(e) {
-    let t = document.querySelector('.tempo');
-    if (start === e.target) {
-        t.style.color = 'white';
-        start.classList = "clicado";
-    } else {
-        start.classList = 'iniciar';
+    start === e.target ? start.classList = "clicado" :  start.classList = 'iniciar';
+    
+    pause === e.target ? pause.classList = 'clicado' : pause.classList = 'pausar';
+
+    clean === e.target ? clean.classList = 'clicado' : clean.classList = 'zerar';
+
+    if (e.target === temas || e.target === iconePincel) {
+        temasDiv.style.display = 'flex';
+        temas.style.height = '200px';
     };
     
-    if (pause === e.target) {
-        pause.classList = 'clicado';
-        t.style.color = 'rgb(139, 139, 255)';
-    } else {
-        pause.classList = 'pausar';
-    };
-    
-    if (clean === e.target) {
-        clean.classList = 'clicado';
-        t.style.color = 'red';
-    } else {
-        clean.classList = 'zerar';
+    if (e.target === principal || e.target === start || e.target === pause || e.target === clean) {
+        temasDiv.style.display = 'none';
+        temas.style.height = '70px';
     };
 });
 
-document.addEventListener('click', function(e) {
-    let t = document.querySelector('.tempo');
+document.addEventListener('change', function(e) {
     if (e.target === blue) {
-            principal.style.backgroundColor = 'var(--darkblue)';
-            conteudo.style.backgroundColor = 'var(--lightblue)';
-            timer.style.backgroundColor = 'var(--midleblue)';
-            t.style.backgroundColor = 'var(--lightblue)';
-            const cor = 'blue';
-            localStorage.setItem('teme', JSON.stringify([cor]));
+        const cor = 'blue';
+        stilos(cor);
+        localStorage.setItem('teme', JSON.stringify([cor]));
     };
 
     if (e.target === pink) {
-        principal.style.backgroundColor = 'var(--darkpink)';
-        conteudo.style.backgroundColor = 'var(--lightpink)';
-        timer.style.backgroundColor = 'var(--midlepink)';
-        t.style.backgroundColor = 'var(--lightpink)';
         const cor = 'pink';
+        stilos(cor);
         localStorage.setItem('teme', JSON.stringify([cor]));
     };
 
     if (e.target === dark) {
-        principal.style.backgroundColor = 'var(--darkblack)';
-        conteudo.style.backgroundColor = 'var(--lightblack)';
-        timer.style.backgroundColor = 'var(--midleblack)';
-        t.style.backgroundColor = 'var(--lightblack)';
-        const cor = 'dark';
+        const cor = 'black';
+        stilos(cor);
         localStorage.setItem('teme', JSON.stringify([cor]));
-    };
-
-    if (e.target === temas || e.target === temas2) {
-        temasDiv[0].style.display = 'flex';
-        temasDiv[1].style.display = 'flex';
-        temasDiv[2].style.display = 'flex';
-        temasDiv[3].style.display = 'flex';
-        temas.style.height = '200px';
     };
 });
 
 
+function stilos(a) {
+    let t = document.querySelector('.tempo');
+    principal.style.backgroundColor = `var(--dark${a})`;
+    conteudo.style.backgroundColor = `var(--light${a})`;
+    timer.style.backgroundColor = `var(--midle${a})`;
+    t.style.backgroundColor = `var(--light${a})`;
+}
 
 function temaModo() {
     const modoTema = localStorage.getItem('teme');
     const p = JSON.parse(modoTema);
     let t = document.querySelector('.tempo');
-    console.log(modoTema)
-    if (p[0] === 'dark') {
-        principal.style.backgroundColor = 'var(--darkblack)';
-        conteudo.style.backgroundColor = 'var(--lightblack)';
-        timer.style.backgroundColor = 'var(--midleblack)';
-        t.style.backgroundColor = 'var(--lightblack)';
-    };
-
-    if (p[0] === 'blue') {
-        principal.style.backgroundColor = 'var(--darkblue)';
-        conteudo.style.backgroundColor = 'var(--lightblue)';
-        timer.style.backgroundColor = 'var(--midleblue)';
-        t.style.backgroundColor = 'var(--lightblue)';
-    };
-    
-    if (p[0] === 'pink') {
-        principal.style.backgroundColor = 'var(--darkpink)';
-        conteudo.style.backgroundColor = 'var(--lightpink)';
-        timer.style.backgroundColor = 'var(--midlepink)';
-        t.style.backgroundColor = 'var(--lightpink)';
-    };
+    stilos(p[0]);
 }
 
 temaModo();
