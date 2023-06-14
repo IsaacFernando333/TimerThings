33409710df0f497d7052d28d75c409fcf2c644f0
Isaac Fernando <isaacfernandonovo@gmail.com>
2023-06-14 18:39:33 -0300

    Armazenei foto no localstorage

--- Criar-user.js
+++ Criar-user.js
@@ -4,20 +4,45 @@ const telavalor = document.querySelector('.novoUser');
 const foto = document.querySelector('.novoUser-foto');
 const btn = document.querySelector('.novoUser-btn');
 const mostrarNome = document.querySelector('.nome-valor');
-// const perfil = document.querySelector('.foto');
+const perfil = document.querySelector('.foto');
 
 btn.addEventListener('click', function() {
+
+    const fr = new FileReader();
+    if (foto.value !== '') {
+        fr.readAsDataURL(foto.files[0]);
+    };
+    fr.addEventListener('load', () => {
+        const url = fr.result;
+        const img = new Image();
+        img.src = url;
+        localStorage.setItem('img', url);
+    });
+
     const nome = document.querySelector('.novoUser-nome');
     if (nome.value === '') {
         return;
     } else {
         const valorNome = nome.value;
-        // const fot = foto.value;
-        // localStorage.setItem('img', JSON.stringify(fot));
         localStorage.setItem('user', JSON.stringify(valorNome));
         location.reload();
     }
 });
+
+function garanteFotoUser() {
+    const img = new Image();
+    if (localStorage.img) {
+        const FotoPerfil = localStorage.getItem('img');
+        img.src = FotoPerfil;
+        perfil.appendChild(img);
+    } else {
+        const imgPlaceHolder = document.createElement('img');
+        imgPlaceHolder.src = "./img/istockphoto-1300845620-612x612.jpg";
+        imgPlaceHolder.alt ='Rosto';
+        perfil.appendChild(imgPlaceHolder);
+    };
+}
+
 
 function garanteNomeUser() {
     if (localStorage.getItem('user') === null) {
@@ -30,6 +55,7 @@ function garanteNomeUser() {
         // const picture = JSON.parse(localStorage.getItem('img'));
         // perfil.setAttribute('src', `C:/Users/isaac/Desktop/${picture}`);
         mostrarNome.innerHTML = valorNomeVolta;
+        garanteFotoUser();
     }
 }
 
